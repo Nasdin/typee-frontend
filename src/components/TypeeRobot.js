de94f2412ad9d5966582c8362bd7976a7ef84388
Nasrudin <Nasrudin.Salim.Suden@gmail.com>
2023-03-20 02:51:32 +0800

    Typee the keyboard should have hands

--- src/components/TypeeRobot.js
+++ src/components/TypeeRobot.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Image, StyleSheet } from 'react-native';
 
-const TypeeRobot = () => {
+const Robot = () => {
+  const [leftHandPosition, setLeftHandPosition] = useState({ x: 0, y: 0 });
+  const [rightHandPosition, setRightHandPosition] = useState({ x: 0, y: 0 });
+
+  const handleKeyPress = (key) => {
+    // Calculate the coordinates of the key on screen
+    const keyCoordinates = calculateKeyCoordinates(key);
+
+    // Calculate the distance between each hand and the key
+    const leftHandDistance = calculateDistance(leftHandPosition, keyCoordinates);
+    const rightHandDistance = calculateDistance(rightHandPosition, keyCoordinates);
+
+    // Move the closest hand to the key
+    if (leftHandDistance <= rightHandDistance) {
+      setLeftHandPosition(keyCoordinates);
+    } else {
+      setRightHandPosition(keyCoordinates);
+    }
+  };
+
   return (
     <View style={styles.robotContainer}>
+      <View
+        style={[styles.hand, { left: leftHandPosition.x, top: leftHandPosition.y }]}
+      />
+      <View
+        style={[styles.hand, { right: rightHandPosition.x, top: rightHandPosition.y }]}
+      />
       <Image
         source={require('../assets/typee_robot.png')}
         style={styles.robotImage}
@@ -17,12 +42,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginBottom: 20,
+    position: 'relative',
   },
   robotImage: {
     width: 150,
     height: 150,
     resizeMode: 'contain',
   },
+  hand: {
+    position: 'absolute',
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    backgroundColor: 'gray',
+  },
 });
 
-export default TypeeRobot;
+export default Robot;
